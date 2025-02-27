e5ccf3ebd3e66bf34cafb6b66a79ab6890350dab
Jesus Castro <castrojesus39@gmail.com>
2025-02-26 22:57:48 -0800

    added service via AddApplication method from ApplicationServicesCollectionExtensions file

--- Movies.API/Program.cs
+++ Movies.API/Program.cs
@@ -1,3 +1,5 @@
+using Movies.Application;
+
 namespace Movies.API;
 
 public class Program
@@ -12,6 +14,8 @@ public class Program
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
 
+        builder.Services.AddApplication();
+        
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
