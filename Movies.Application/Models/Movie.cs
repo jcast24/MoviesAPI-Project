cc2af5841b4ed8e61d9f073567f49d93d059b372
Jesus Castro <castrojesus39@gmail.com>
2025-02-26 22:56:07 -0800

    model structure of how the in-memory database is going to look like

---
+++ Movies.Application/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace Movies.Application.Models;
+
+public class Movie
+{
+    public required Guid Id { get; init; }
+    public required string Title { get; set; }
+    public required int YearOfRelease { get; set; }
+    
+    // this approach allows to add items during runtime because 
+    // it is a list mutable data structure
+    public required List<string> Genres { get; init; } = [];
+}
