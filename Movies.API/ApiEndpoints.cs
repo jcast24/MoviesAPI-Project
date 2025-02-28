6673edbd07a15623152301f8749cd9706a1ba5a4
Jesus Castro <castrojesus39@gmail.com>
2025-02-27 20:03:37 -0800

    created endpoints for Get and GetAll

--- Movies.API/ApiEndpoints.cs
+++ Movies.API/ApiEndpoints.cs
@@ -9,5 +9,10 @@ public static class ApiEndpoints
         private const string Base = $"{ApiBase}/Movies";
 
         public const string Create = Base;
+        
+        // :guid is a constraint on the route. only accepts guid
+        public const string Get = $"{Base}/{{id:guid}}";
+
+        public const string GetAll = Base;
     }
 }
