e4e224bb1caec78b6e286b892e74814e54f63052
Jesus Castro <castrojesus39@gmail.com>
2025-02-27 20:04:36 -0800

    MovieResponse method MapToResponse made to return MovieRepsonse

--- Movies.API/Mapping/ContractMapping.cs
+++ Movies.API/Mapping/ContractMapping.cs
@@ -1,5 +1,6 @@
 ﻿using Movies.Application.Models;
 using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
 
 namespace Movies.API.Mapping;
 
@@ -15,6 +16,17 @@ public static class ContractMapping
             Title = request.Title,
             YearOfRelease = request.YearOfRelease,
             Genres = request.Genres.ToList()
+        };
+    }
+
+    public static MovieResponse MapToResponse(this Movie movie)
+    {
+        return new MovieResponse
+        {
+            Id = movie.Id,
+            Title = movie.Title,
+            YearOfRelease = movie.YearOfRelease,
+            Genres = movie.Genres
         };
     }
 }
