5ba64dd99404fde9cf375606d635f9e6feeac65e
Jesus Castro <castrojesus39@gmail.com>
2025-02-27 19:55:45 -0800

    new file to keep track of API endpoints

---
+++ Movies.API/ApiEndpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace Movies.API;
+
+public static class ApiEndpoints
+{
+    private const string ApiBase = "api";
+
+    public static class Movies
+    {
+        private const string Base = $"{ApiBase}/Movies";
+
+        public const string Create = Base;
+    }
+}
