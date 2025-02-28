a1e73ed247456812fe6dc4a12bf325e8256329c8
Jesus Castro <castrojesus39@gmail.com>
2025-02-27 19:51:12 -0800

    created MoviesController + created constructor and the POST method Create()

---
+++ Movies.API/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.API.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.API.Controllers;
+
+[ApiController]
+[Route("api")]
+public class MoviesController : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MoviesController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+    
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        var movie = request.MapToMovie();
+        await _movieRepository.CreateAsync(movie);
+        return Created($"/api/movies/{movie.Id}", movie); // Should return 201 created
+    }
+}
