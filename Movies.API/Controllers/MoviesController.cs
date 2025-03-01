8ece265961e09df6f96195ed6cfcdbc634503099
Jesus Castro <castrojesus39@gmail.com>
2025-02-28 21:39:23 -0800

    Implemented update method

--- Movies.API/Controllers/MoviesController.cs
+++ Movies.API/Controllers/MoviesController.cs
@@ -46,5 +46,20 @@ public class MoviesController : ControllerBase
         var moviesResponse = movies.MapToResponse();
         return Ok(moviesResponse);
     }
+
+    [HttpPut(ApiEndpoints.Movies.Update)]
+    public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateMovieRequest request)
+    {
+        var movie = request.MapToMovie(id);
+        var updated = await _movieRepository.UpdateAsync(movie);
+
+        if (!updated)
+        {
+            return NotFound();
+        }
+
+        var response = movie.MapToResponse();
+        return Ok(response);
+    }
     
 }
