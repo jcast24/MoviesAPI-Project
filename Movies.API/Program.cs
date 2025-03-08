86126e330f69677f1ffe77e4fe42e2035a3890b1
Jesus Castro <castrojesus39@gmail.com>
2025-03-07 22:53:06 -0800

    Changed entire file to top level statement

--- Movies.API/Program.cs
+++ Movies.API/Program.cs
@@ -1,38 +1,33 @@
 using Movies.Application;
+using Movies.Application.Database;
 
-namespace Movies.API;
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
-public class Program
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddApplication();
+builder.Services.AddDatabase(config["Database:ConnectionString"]);
+    
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-        var config = builder.Configuration;
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
-
-        builder.Services.AddApplication();
-        builder.Services.AddDatabase(config["Database:ConnectionString"]);
-        
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
-    }
+    app.MapOpenApi();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+var dbInitializer = app.Services.GetRequiredService<DbInitializer>();
+await dbInitializer.InitializeAsync();
+
+app.Run();
