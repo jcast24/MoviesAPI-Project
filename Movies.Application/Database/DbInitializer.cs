0ae762d2332a092986df392b1fabf9bfda588ea5
Jesus Castro <castrojesus39@gmail.com>
2025-03-07 22:52:46 -0800

    Re-did formatting of Dapper statements

--- Movies.Application/Database/DbInitializer.cs
+++ Movies.Application/Database/DbInitializer.cs
@@ -1,6 +1,6 @@
-namespace Movies.Application.Database;
 using Dapper;
 
+namespace Movies.Application.Database;
 
 public class DbInitializer
 {
@@ -16,17 +16,17 @@ public class DbInitializer
         using var connection = await _dbConnectionFactory.CreateConnectionAsync();
 
         await connection.ExecuteAsync("""
-                create table if not exists movies (
-                    id UUID primary key, 
-                    slug TEXT not null, 
-                    title TEXT not null, 
-                    yearofrelease integer not null
-                    );
-                """);
+                                          create table if not exists movies (
+                                          id UUID primary key,
+                                          slug TEXT not null, 
+                                          title TEXT not null,
+                                          yearofrelease integer not null);
+                                      """);
+        
         await connection.ExecuteAsync("""
-                create unique index concurrently if not exists movies_slug_idx
-                on movies
-                using btree(slug);
-                """);
+                                          create unique index concurrently if not exists movies_slug_idx
+                                          on movies
+                                          using btree(slug);
+                                      """);
     }
 }
