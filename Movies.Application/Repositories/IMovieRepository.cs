86c05831cfbd50dd6f8f75324fe45dd000c6e14e
Jesus Castro <castrojesus39@gmail.com>
2025-03-13 22:04:14 -0700

    added userId parameter to GetById, GetBySlug, GetAll, and Update

--- Movies.Application/Repositories/IMovieRepository.cs
+++ Movies.Application/Repositories/IMovieRepository.cs
@@ -5,11 +5,11 @@ namespace Movies.Application.Repositories;
 public interface IMovieRepository
 {
     Task<bool> CreateAsync(Movie movie, CancellationToken token = default);
-    Task<Movie?> GetByIdAsync(Guid id, CancellationToken token = default);
+    Task<Movie?> GetByIdAsync(Guid id, Guid? userid = default, CancellationToken token = default);
 
-    Task<Movie?> GetBySlug(string slug, CancellationToken token = default);
-    Task<IEnumerable<Movie>> GetAllAsync(CancellationToken token = default);
-    Task<bool> UpdateAsync(Movie movie, CancellationToken token = default);
+    Task<Movie?> GetBySlug(string slug, Guid? userid = default, CancellationToken token = default);
+    Task<IEnumerable<Movie>> GetAllAsync(Guid? userid = default, CancellationToken token = default);
+    Task<bool> UpdateAsync(Movie movie, Guid? userid = default, CancellationToken token = default);
     Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default);
 
     Task<bool> ExistsByIdAsync(Guid id, CancellationToken token = default);
