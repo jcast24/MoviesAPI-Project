1f0bd65dfff515a345eae42361efdd19554284c3
Jesus Castro <castrojesus39@gmail.com>
2025-03-16 16:50:36 -0700

    updated GetAllAsync to take in the GetAllMovieOptions options and updated the sql

--- Movies.Application/Repositories/MovieRepository.cs
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -91,7 +91,7 @@ public class MovieRepository : IMovieRepository
         return movie;
     }
 
-    public async Task<IEnumerable<Movie>> GetAllAsync(Guid? userId = default, CancellationToken token = default)
+    public async Task<IEnumerable<Movie>> GetAllAsync(GetAllMoviesOptions options, CancellationToken token = default)
     {
         using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
         var result = await connection.QueryAsync(
@@ -105,8 +105,11 @@ public class MovieRepository : IMovieRepository
                                   left join ratings r on m.id = r.movieid
                                   left join ratings myr on m.id = myr.movieid
                                     and myr.userid = @userId
-                                  group by id
-                                  """, new {userId} ,cancellationToken: token));
+                                  where (@title is null or m.title like ('%' || @title || '%'))
+                                  and (@yearofrelease is null or m.yearofrelease = @yearofrelease)
+                                  group by id, userrating
+                                  """, 
+                new {userId = options.UserId, title = options.Title, yearofrelease = options.YearOfRelease}, cancellationToken: token));
 
         return result.Select(x => new Movie
         {
