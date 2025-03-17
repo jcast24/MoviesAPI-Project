79a7cc00960f4d114a87e4e5de6657c2a984d653
Jesus Castro <castrojesus39@gmail.com>
2025-03-16 22:59:41 -0700

    created/implemented GetCountAsync and added the necessary sql statement

--- Movies.Application/Repositories/IMovieRepository.cs
+++ Movies.Application/Repositories/IMovieRepository.cs
@@ -11,6 +11,6 @@ public interface IMovieRepository
     Task<IEnumerable<Movie>> GetAllAsync(GetAllMoviesOptions options, CancellationToken token = default);
     Task<bool> UpdateAsync(Movie movie, CancellationToken token = default);
     Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default);
-
     Task<bool> ExistsByIdAsync(Guid id, CancellationToken token = default);
+    Task<int> GetCountAsync(string? title, int? yearOfRelease, CancellationToken token = default);
 }
